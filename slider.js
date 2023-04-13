495196c2a4312e26479d91d3b7c147c85831fc20
SiljeP <u2440913@rts-365.dk>
2023-04-13 12:50:58 +0200

    NU VIRKER MIN SLIDER

--- slider.js
+++ slider.js
@@ -4,44 +4,57 @@ const SLIDER_IMG = document.querySelectorAll(".inner__slider img");
 const PREV_BTN = document.querySelector("#prevBtn");
 const NEXT_BTN = document.querySelector("#nextBtn");
 
-let index = 1;
-const SIZE = SLIDER_IMG[1].clientWidth;
+let index = 0;
+const SIZE = SLIDER_IMG[0].clientWidth;
 
 
 //starter paa slide 1 i stedet for 0
 INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px';
 
+
+//KNAPPERNE
+
 NEXT_BTN.addEventListener("click", mover);
 
-function mover(event){
-     if(index >= SLIDER_IMG.length -1) return;
+function mover(event) {
     index++;
-    INNER_SLIDER.style.transition = "transform 0.4s ease";
-    INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)'; 
-}
-    
- PREV_BTN.addEventListener("click", moverBack);
+    if (index >= SLIDER_IMG.length) {
+        index = 0
+        INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+        return
 
-function moverBack(event){
-     if(index <= 0) return;
-    INNER_SLIDER.style.transition = "transform 0.4s ease";
-    index--;
-    INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
     }
-    
- INNER_SLIDER.addEventListener('transitionend', tranEnd);
 
-  function tranEnd(event){
-      if (SLIDER_IMG [index].id === 'lastClone')
-      //INNER_SLIDER.style.transition = "none";
-      index = SLIDER_IMG.length - index;
-      INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+    INNER_SLIDER.style.transition = "transform 1s ease";
+    INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+}
 
-      if (SLIDER_IMG [index].id === 'firstClone')
-      //INNER_SLIDER.style.transition = "none";
-      index = SLIDER_IMG.length - index;
-      INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
-  }
+PREV_BTN.addEventListener("click", moverBack);
+
+function moverBack(event) {
+    index--;
+    if (index <= 0) index = SLIDER_IMG.length -1;
+
+    INNER_SLIDER.style.transition = "transform 1s ease";
+    INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+}
+
+//NÅR ANIMATIONEN/TRANSITION ER FÆRDIG
+
+// INNER_SLIDER.addEventListener('transitionend', tranEnd);
+
+// function tranEnd(event) {
+//     if (SLIDER_IMG[index].id === 'lastClone') {
+//         INNER_SLIDER.style.transition = "none";
+//         index = SLIDER_IMG.length - 1;
+//         INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+//     }
+//     if (SLIDER_IMG[index].id === 'firstClone') {
+//         INNER_SLIDER.style.transition = "none";
+//         index = SLIDER_IMG.length - 1;
+//         INNER_SLIDER.style.transform = 'translateX(' + (-SIZE * index) + 'px)';
+//     }
+// }
 
     //Fordi den ene function (tranEnd) først starter når transition SLUTTER så kan man klikke hurtigt på piletasterne og komme udenom den og det er grimt. Derfor tilføjes if(index <= 0) return;
 
@@ -54,4 +67,3 @@ function moverBack(event){
     // } else {
     //     SLIDER_IMG.classList.add("animatedDiv--moved")
     // }
- 
